77fadf0c37c80d6d494feffb93d0df9c2a4ea37c
ElktrumElk <festinacole373@gmail.com>
2026-02-07 23:36:35 +0000

    New style update

--- script.js
+++ script.js
@@ -2,9 +2,38 @@ const card = document.getElementById("card");
 const main = document.getElementById("main");
 const quickAccessCnt = document.getElementById("q_a_c");
 const navMarker = document.getElementById("nav_marker");
+const menuBtn = document.getElementById("acc_menu");
+const menuClsBtn = document.getElementById("sidebar_close");
+const sideBar = document.getElementById("side_bar");
+const sideBarDis = document.getElementById("dis");
+
+const qAnimation = quickAccessCnt.getAnimations()[0];
 
 
-const qAnimation = quickAccessCnt.getAnimations()[0]
+/**
+ * Function that close the side bar
+ */
+function closeSideBar() {
+    if (isMenuOpened) {
+        sideBar.style.transform = "translateX(140%)";
+        sideBarDis.style.opacity = 0;
+    }
+
+    setTimeout(() => {
+
+        sideBarDis.style.display = "none";
+
+    }, 400);
+    document.body.style.overflowY = "";
+
+    isMenuOpened = false;
+}
+
+
+
+
+
+
 
 quickAccessCnt.addEventListener("click", () => {
 
@@ -47,7 +76,10 @@ quickAccessCnt.addEventListener("touchmove", (e) => {
         if (moveX > (rightPos - markerWidth - 10) || moveX < 0) return;
 
         console.log(moveX, rightPos)
-        navMarker.style.transform = `translateX(${moveX}px) scaleX(1.2)`;
+        requestAnimationFrame(() => {
+            navMarker.style.transform = `translateX(${moveX}px) scaleX(1.2)`;
+        })
+
         currentPos = moveX;
     }
 });
@@ -73,9 +105,36 @@ document.body.addEventListener("touchend", () => {
     }
 })
 
+//demo
 for (let i = 0; i < 20; i++) {
     const newCard = card.cloneNode(true);
 
     main.appendChild(newCard);
 }
+
+/**=====================SIDEBAR SECTION=============================== */
+let isMenuOpened = false;
+
+/**Open side bar */
+menuBtn.addEventListener("click", () => {
+    if (!isMenuOpened) {
+        sideBarDis.style.display = "flex";
+
+        document.body.style.overflowY = "hidden"
+
+        requestAnimationFrame(() => {
+            sideBar.style.transform = "translateX(25%)";
+            sideBarDis.style.opacity = 1;
+        });
+        isMenuOpened = true;
+    }
+})
+
+sideBarDis.addEventListener("click", () => {
+    closeSideBar();
+});
+/**Close side bar */
+menuClsBtn.addEventListener("click", () => {
+    closeSideBar();
+});
 
