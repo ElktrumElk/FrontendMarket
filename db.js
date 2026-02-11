9c11dfc6ceaf88e022daed2fd1a16e902497bcf5
ElktrumElk <festinacole373@gmail.com>
2026-02-11 11:15:09 +0000

    changes made

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 export const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'EfbjOelktrumelk@1234',
+    password: 'my pass',
     database: 'frontendUsers'
 });
 
