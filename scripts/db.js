c06a1245fdebe893e7a8bd034b90ec9ceb96f931
Elktrum Elk <131537619+ElktrumElk@users.noreply.github.com>
2026-02-20 22:25:45 +0000

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,18 +1,19 @@
 import mysql from 'mysql2/promise';
 import dotenv from "dotenv";
 
+/*
 dotenv.config({
     path: "../../.env",
     quiet: true
 });
-
+*/
 
 console.log("o", process.env.DB_KEY);
 export const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_KEY,
-    database: 'frontendUsers'
+    database: process.env.DB_NAME
 });
 
 export default db;
