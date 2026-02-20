1c094bd0daa8d9bbae9b1d99109abb2210f8c914
ElktrumElk <festinacole373@gmail.com>
2026-02-20 09:33:15 +0000

    login try

--- public/script/login.js
+++ public/script/login.js
@@ -4,35 +4,43 @@ const loginForm = document.getElementById("loginForm");
 
 loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
+    try {
 
-    const formData = new FormData(loginForm);
-    const jsonData = Object.fromEntries(formData.entries());
 
-    const res = await fetch('/login', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(jsonData)
-    });
+        const formData = new FormData(loginForm);
+        const jsonData = Object.fromEntries(formData.entries());
 
-    const data = await res.json();
-
-    if (data.state === true) {
-
-        notificationPanel({
-            message: data.message,
-            type: "success"
+        const res = await fetch('/login', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(jsonData)
         });
 
-        setTimeout(() => {
-            window.location.href = "/home";
-        }, 1000);
+        const data = await res.json();
 
-    } else {
+        if (data.state === true) {
 
+            notificationPanel({
+                message: data.message,
+                type: "success"
+            });
+
+            setTimeout(() => {
+                window.location.href = "/home";
+            }, 1000);
+
+        } else {
+
+            notificationPanel({
+                message: data.message,
+                type: "error"
+            });
+        }
+    } catch (e) {
         notificationPanel({
-            message: data.message,
+            message: "There was a problem connecting to the server. Please try again later",
             type: "error"
         });
     }
