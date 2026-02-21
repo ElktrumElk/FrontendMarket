452e657d096869f554ddf93e00b2d5d323b239b5
Elktrum Elk <131537619+ElktrumElk@users.noreply.github.com>
2026-02-21 00:26:41 +0000

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -9,6 +9,12 @@ dotenv.config({
 */
 
 console.log("o", process.env.DB_KEY);
-export const db = mysql.createPool(process.env.DBURL);
+export const db = mysql.createPool({
+      host: process.env.MYSQLHOST,
+      user: process.env.MYSQLUSER,
+      password: process.env.MYSQLPASSWORD,
+      database: process.env.MYSQLDATABASE,
+      port: process.env.MYSQLPORT,
+    });
 
 export default db;
