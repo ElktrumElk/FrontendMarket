85d49542ee00e2aadaf1b82506564d7b8b4e7b5d
Elktrum Elk <131537619+ElktrumElk@users.noreply.github.com>
2026-02-21 08:34:06 +0000

    Update server1.js

--- scripts/server/server1.js
+++ scripts/server/server1.js
@@ -309,7 +309,7 @@ app.get("/get/posts", async (req, res) => {
         console.log(res[1]);
     }
 });
-const PORT = process.env.PORT;
-app.listen(PORT || 3000, "0.0.0.0", () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port {PORT}`);
 })
