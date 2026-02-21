3ebc9366026b4f66a540199e2979a59b832c3161
Elktrum Elk <131537619+ElktrumElk@users.noreply.github.com>
2026-02-21 01:40:06 +0000

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -10,7 +10,7 @@ dotenv.config({
 
 console.log("o", process.env.DB_KEY);
 const db = mysql.createPool({
-      host: "maglev.proxy.rlwy.net",
+      host: "mysql.railway.internal",
       user: process.env.MYSQLUSER,
       password: process.env.MYSQLPASSWORD,
       database: process.env.MYSQLDATABASE,
